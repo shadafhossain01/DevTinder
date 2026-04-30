ebc42b61ff293529ce18282724bd4923762f8118
Shadaf Hossain <shadafhossain01@gmail.com>
2026-05-01 01:07:41 +0600

    Routing Done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,68 @@
-import { createRoot } from 'react-dom/client'
+import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
-import './index.css'
-import App from './App.jsx'
-import Login from './pages/Login.jsx';
-import Profile from './pages/Profile.jsx';
-import Home from './pages/Home.jsx';
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App.jsx";
+import Login from "./pages/Login.jsx";
+import Profile from "./pages/Profile.jsx";
+import Home from "./pages/Home.jsx";
+import { store } from "./store/store.js";
+import Connections from "./pages/Connections.jsx";
+import Requests from "./pages/Requests.jsx";
+import Signup from "./pages/Signup.jsx";
+import ProtectedRoute from "./components/common/ProtectedRoute.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} > 
-      <Route path="/" element={<Home/>}/> 
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/profile' element={<Profile/>}/>
-      </Route>
-    </Routes>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/" element={<App />}>
+
+          {/* Protected Route */}
+          <Route
+            index
+            element={
+              <ProtectedRoute>
+                {" "}
+                <Home />{" "}
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                {" "}
+                <Profile />{" "}
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/connections"
+            element={
+              <ProtectedRoute>
+                {" "}
+                <Connections />{" "}
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/requests"
+            element={
+              <ProtectedRoute>
+                {" "}
+                <Requests />{" "}
+              </ProtectedRoute>
+            }
+          />
+
+          {/* Auth Route */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </Provider>
 );
